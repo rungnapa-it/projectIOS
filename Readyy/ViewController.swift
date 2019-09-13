649ce01789ccea7179e9b53ec794a12333445c69
rungnapa <rungnapa.it@ku.th>
2019-09-13 23:57:12 +0700

    first commit

---
+++ Readyy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Readyy
+//
+//  Created by Ratsuda Suwan on 12/9/2562 BE.
+//  Copyright © 2562 Ratsuda Suwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
