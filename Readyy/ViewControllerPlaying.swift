dfb79ae55315815394db357ee268f58842d6af2f
rungnapa <rungnapa.it@ku.th>
2019-10-12 00:33:45 +0700

    update

--- Readyy/ViewControllerPlaying.swift
+++ Readyy/ViewControllerPlaying.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewControllerPlaying: UIViewController {
     
-  
+    var buttonNext = false
+    
     @IBOutlet weak var questionField: UITextField!
     @IBOutlet weak var choices1Button: UIButton!
     
@@ -25,22 +26,51 @@ class ViewControllerPlaying: UIViewController {
     }
     
 
-
-    func ready () {
-        let dictionary = choices.getDictionary()
-        for (key,value) in dictionary{
-            
-            let qu = question.getNameIndex(id: key)
-            print(qu)
-            for (name) in value {
-                print(name)
-            }
-            
-            
-        }
+    @IBAction func setButton(_ sender: Any) {
+        buttonNext = true
     }
-
-
-
+    
+    func ready () {
+        var dictionary = choices.getDictionary()
+        for (i) in 0...2 {
+            
+                var array = [String]()
+                var id = accessToDictionary(dict: dictionary)
+                self.questionField.text = question.getNameIndex(id: id)
+                for (key , value) in dictionary {
+                        if (key == id){
+                            for (v) in value{
+                                array.append(v)
+                            }
+                        }
+                    }
+                //dictionary[id] = nil
+                var choices1 = array[0]
+                var choices2 = array[1]
+                var choices3 = array[2]
+                var choices4 = array[3]
+                choices1Button.setTitle("\(choices1)", for: .normal)
+                choices2Button.setTitle("\(choices2)", for: .normal)
+                choices3Button.setTitle("\(choices3)", for: .normal)
+                choices4Button.setTitle("\(choices4)", for: .normal)
+                dictionary[id] = nil
+            
+            
+            }
+        }
+        
+    
+        
+        
+    
+    
+    func accessToDictionary(dict:[String:Array<String>]) -> String {
+        var keyQuestion = ""
+        for (key,value) in dict{
+            keyQuestion = key
+            break
+        }
+        return keyQuestion
+    }
 }
 
