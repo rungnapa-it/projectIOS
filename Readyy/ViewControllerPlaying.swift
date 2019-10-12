d9a8bc2ba053a4fa5f538bde640ca1639179fb80
rungnapa <rungnapa.it@ku.th>
2019-10-13 00:36:11 +0700

    updateplaying

--- Readyy/ViewControllerPlaying.swift
+++ Readyy/ViewControllerPlaying.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class ViewControllerPlaying: UIViewController {
-    
-    var buttonNext = false
+    var dictionary = choices.getDictionary()
+    var buttonNext = true
+    var buttonchoices1 = false
+    var buttonchoices2 = false
+    var buttonchoices3 = false
+    var buttonchoices4 = false
+   
     
     @IBOutlet weak var questionField: UITextField!
     @IBOutlet weak var choices1Button: UIButton!
@@ -19,45 +24,74 @@ class ViewControllerPlaying: UIViewController {
     @IBOutlet weak var choices3Button: UIButton!
     @IBOutlet weak var choices2Button: UIButton!
     override func viewDidLoad() {
-        
+        dictionary = choices.getDictionary()
+        buttonchoices1 = true
         ready()
         question.getDictionary()
         choices.getDictionary()
+        
+    }
+    @IBAction func setChoices1(_ sender: Any) {
+        buttonchoices1 = true
+        ready()
+    }
+    @IBAction func setChoices2(_ sender: Any) {
+        buttonchoices2 = true
+        ready()
+    }
+    @IBAction func setChoices3(_ sender: Any) {
+        buttonchoices3 = true
+        ready()
+    }
+    @IBAction func setChoices4(_ sender: Any) {
+        buttonchoices4 = true
+        ready()
     }
     
 
     @IBAction func setButton(_ sender: Any) {
         buttonNext = true
+        ready()
+        
     }
     
     func ready () {
-        var dictionary = choices.getDictionary()
-        var array = [String]()
-        var id = accessToDictionary(dict: dictionary)
-        self.questionField.text = question.getNameIndex(id: id)
-        
-        for (key , value) in dictionary {
-            if (key == id){
-                for (v) in value{
-                    array.append(v)
+        while (dictionary.count != 0 && buttonchoices1 == true || buttonchoices2 == true || buttonchoices3 == true || buttonchoices4 == true ){
+           buttonchoices1 = false
+            buttonchoices2 = false
+            buttonchoices3 = false
+            buttonchoices4 = false
+            var array = [String]()
+            var id = accessToDictionary(dict: dictionary)
+            self.questionField.text = question.getNameIndex(id: id)
+            
+            for (key , value) in dictionary {
+                if (key == id){
+                    for (v) in value{
+                        array.append(v)
+                    }
                 }
             }
+            
+            //dictionary[id] = nil
+            print("Aictionary : \(dictionary)")
+            print("Array : \(array)")
+            if (array.count == 4){
+                var choices1 = array[0]
+                var choices2 = array[1]
+                var choices3 = array[2]
+                var choices4 = array[3]
+                choices1Button.setTitle("\(choices1)", for: .normal)
+                choices2Button.setTitle("\(choices2)", for: .normal)
+                choices3Button.setTitle("\(choices3)", for: .normal)
+                choices4Button.setTitle("\(choices4)", for: .normal)
+            }
+            dictionary[id] = nil
         }
-        
-        //dictionary[id] = nil
-        var choices1 = array[0]
-        var choices2 = array[1]
-        var choices3 = array[2]
-        var choices4 = array[3]
-        choices1Button.setTitle("\(choices1)", for: .normal)
-        choices2Button.setTitle("\(choices2)", for: .normal)
-        choices3Button.setTitle("\(choices3)", for: .normal)
-        choices4Button.setTitle("\(choices4)", for: .normal)
-        //dictionary[id] = nil
         
             
         
-        }
+    }
         
     
         
